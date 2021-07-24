deae7873c17be34f91c132fe3326a1e5b04d13b0
Sandurov Harbi <habi.alonso98@gmail.com>
2021-07-24 17:56:55 +0600

    install tailwindcss

--- client/src/components/ProductsCard.vue
+++ client/src/components/ProductsCard.vue
@@ -6,6 +6,11 @@
 
 <script>
     export default {
-        name: 'ProductCard'
+        name: 'ProductCard',
+        props: {
+            data: {
+                type: Object
+            }
+        }
     }
 </script>
